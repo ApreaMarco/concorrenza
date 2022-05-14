3627c125cd827ec0bfe9d6ebd132630e008907b8
ApreaMarco <105546344+ApreaMarco@users.noreply.github.com>
2022-05-14 13:52:28 +0200

    Add files via upload

---
+++ v1.0/convertitoreSequenziale.py
@@ -0,0 +1,21 @@
+from convertitore import *
+from time import time
+
+
+def main():
+    t0 = time()
+    tipoProduttore = [produttore1, produttore2, produttore3, produttore4]  # Produttori disponibili
+    produttoreTest = tipoProduttore[0]  # Produttore scelto per il test
+    tipoMyPrint = [1, 2, 3]  # Stampe disponibili
+    myPrintTest = tipoMyPrint[0]  # Tipo di stampa scelto per il test
+
+    for i in range(1, 11):
+        # QUI IL CODICE SPECIFICO PER L’IMPLEMENTAZIONE SCELTA
+        produttoreTest(i, myPrintTest)
+
+    tn = time()
+    myPrint(f"Tempo di esecuzione: {tn - t0}", myPrintTest)
+
+
+if __name__ == "__main__":
+    main()
